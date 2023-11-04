ad2ca202fccf1963d2004776b1aa7a2e570d7d07
andreevgs <andreev.gs12@yandex.by>
2023-11-05 02:39:23 +0300

    exp: set base

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "qr-sticks",
   plugins: [
     vue({
       template: { transformAssetUrls },
