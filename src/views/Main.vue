1091ee63227db969e34fb6e8bdaee610c47b37f0
andreevgs <andreev.gs12@yandex.by>
2023-11-05 03:02:43 +0300

    exp: change link

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn variant="tonal" to="/create">Создать новый</v-btn>
+  <v-btn variant="tonal" to="/qr-sticks/create">Создать новый</v-btn>
 </template>
 
 <script lang="ts" setup></script>
